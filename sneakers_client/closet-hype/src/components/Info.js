360c860a46fcb85be300a71a1a82fab94fc69db1
ggatling <gerda.gatling@gmail.com>
2019-11-14 00:30:00 -0500

    Added card to profile for user information

--- sneakers_client/closet-hype/src/components/Info.js
+++ sneakers_client/closet-hype/src/components/Info.js
@@ -1,18 +1,31 @@
 import React from "react";
 import {
-  Row, Col, Container
+  Row, Col, Container, Card
 } from 'react-bootstrap';
 
 const Info = props => {
   return(
-    <Container className="profile">
-      <Row>
-        <Col>{props.email}</Col>
-        <Col>{props.city}</Col>
-        <Col>{props.state}</Col>
-        <Col>{props.zipcode}</Col>
-      </Row>
- </Container>
+
+  <Card style={{ width: '18rem' }}>
+    <Card.Body>
+      <Card.Title>Closet Information</Card.Title>
+      <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+      <Row><Card.Text> Email: {props.email} </Card.Text></Row>
+      <Row><Card.Text> City: {props.city} </Card.Text></Row>
+      <Row><Card.Text> State: {props.state} </Card.Text></Row>
+      <Row><Card.Text> Zipcode: {props.zipcode} </Card.Text></Row>
+      <Card.Link href="#">Card Link</Card.Link>
+      <Card.Link href="#">Another Link</Card.Link>
+    </Card.Body>
+  </Card>
+ //    <Container className="profile">
+ //      <Row>
+ //        <Col>{props.email}</Col>
+ //        <Col>{props.city}</Col>
+ //        <Col>{props.state}</Col>
+ //        <Col>{props.zipcode}</Col>
+ //      </Row>
+ // </Container>
   )
 }
 
